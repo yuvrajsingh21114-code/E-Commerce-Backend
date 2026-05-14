ec91f0be03b8f4cbb9d999e6df22b4ee2b3f4709
Yuvraj Singh <yuvrajsingh21114@gmail.com>
2026-05-14 23:29:18 +0530

    Added preloaded cart

--- routes/productroutes.js
+++ routes/productroutes.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const products = require('../db/db');
+const Cart = require('../db/db');
 
 app.get('/products',(req,res)=>{
     try{
@@ -12,7 +13,15 @@ app.get('/products',(req,res)=>{
     }
 });
 
-//console.log(products);
-console.log("hello");
+app.get('/api/Cart',(req,res)=>{
+    try{
+        res.json(Cart);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send('ERROR');
+    }
+});
+
 
 module.exports=app;
