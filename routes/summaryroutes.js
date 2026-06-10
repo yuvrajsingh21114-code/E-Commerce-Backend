2396e54c230b7e925cde9de879d1d4539ed20e30
Yuvraj Singh <yuvrajsingh21114@gmail.com>
2026-06-10 18:56:06 +0530

    fixed money formatting

--- routes/summaryroutes.js
+++ routes/summaryroutes.js
@@ -1,40 +1,41 @@
-const express= require('express');
-const app= express();
-const cart= require('../db/cart');
-const orders= require('../db/orders');
+const express = require('express');
+const app = express();
+const cart = require('../db/cart');
+const orders = require('../db/orders');
 
-let totalcost=0,i;
-let shippingcost=cart.length*40;
+app.get('/summary', (req, res) => {
+    try {
+        let totalcost = 0, i;
+        let shippingcost = cart.length * 40;
 
-for(i=0;i<cart.length;i++){
-    totalcost+=cart[i].price;
-}
-
-app.get('/summary',(req,res)=>{
-    try{
+        for (i = 0; i < cart.length; i++) {
+            totalcost += cart[i].price;
+        }
         res.json({
-            subtotal: totalcost+shippingcost,
-            shippingcost: shippingcost,
+            subtotal: (totalcost/100 + shippingcost)*100,
+            shippingcost: shippingcost*100,
             totalitems: cart.length,
             total: totalcost
         });
     }
-    catch(err){
+    catch (err) {
         console.log(err);
-        res.status(500).send('Error');        
+        res.status(500).send('Error');
     }
 });
 
-app.post('/summary',(req,res)=>{
-    try{
-        orders.push(...cart);
-        cart.length=0;
-        res.send("Placed order");
+app.post('/summary', (req, res) => {
+    try {
+        setTimeout(() => {
+            orders.push(...cart);
+            cart.length = 0;
+            res.send("Placed order");
+        }, 500);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         res.status(409).send('Error');
     }
 })
 
-module.exports=app;
+module.exports = app;
